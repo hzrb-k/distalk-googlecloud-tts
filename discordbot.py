15733147978a347d42d61959a67364cacd38ded1
hzrb-k <100966963+hzrb-k@users.noreply.github.com>
2022-03-05 10:40:32 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -94,7 +94,7 @@ async def on_message(message):
                 text = message.content
 
                 # Add author's name
-                text = message.author.name + '、' + text
+#                text = message.author.name + '、' + text
 
                 # Replace new line
                 text = text.replace('\n', '、')
